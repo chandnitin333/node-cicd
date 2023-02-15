1b7e8560a98bea9b420b3698acfcda59d1cfb2ed
Nitin Chandekar <nitin.chandekar@jetsynthesys.com>
2023-02-15 17:22:13 +0530

    docker changes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
-# syntax=docker/dockerfile:1
-FROM node:16-alpine
+
+
+FROM node:16.17
+
 WORKDIR /app
+
 COPY package*.json ./
+
 COPY . .
+
 RUN npm install 
+
+RUN npm run build
+
 EXPOSE 3000
+
 CMD ["node", "dist/app.js"] 
--- src/app.ts
+++ src/app.ts
@@ -7,32 +7,37 @@ const child = spawn('pwd');
 let server =  new Server().app;
 let port = 3000;
 
-child.on('exit', function (code, signal) {
-    console.log('child process exited with ' +
-                `code ${code} and signal ${signal}`);
-  });
+// child.on('exit', function (code, signal) {
+//     console.log('child process exited with ' +
+//                 `code ${code} and signal ${signal}`);
+//   });
 
-if (cluster.isMaster) {
-    console.log(`Master PID ${process.pid} is running`);
+// if (cluster.isMaster) {
+//     console.log(`Master PID ${process.pid} is running`);
   
-    // Get the number of available cpu cores
-    const nCPUs = os.cpus().length;
-    // Fork worker processes for each available CPU core
-    for (let i = 0; i < nCPUs; i++) {
-      cluster.fork();
-    }
+//     // Get the number of available cpu cores
+//     const nCPUs = os.cpus().length;
+//     // Fork worker processes for each available CPU core
+//     for (let i = 0; i < nCPUs; i++) {
+//       cluster.fork();
+//     }
   
-    cluster.on("exit", (worker, code, signal) => {
-      console.log(`Worker PID ${worker.process.pid} died`);
-    });
-  } else {
+//     cluster.on("exit", (worker, code, signal) => {
+//       console.log(`Worker PID ${worker.process.pid} died`);
+//     });
+//   } else {
   
-    server.get("/", (req, res) => {
-      res.send("Node is Running...");
-    });
-    server.listen(port,()=>{
-        console.log(`Server Listen port :`,port);
-        console.log(`Worker PID ${process.pid} started`);
-    })
+//     server.get("/", (req, res) => {
+//       res.send("Node is Running...");
+//     });
+//     server.listen(port,()=>{
+//         console.log(`Server Listen port :`,port);
+//         console.log(`Worker PID ${process.pid} started`);
+//     })
   
-  }
+//   }
+
+  server.listen(port,()=>{
+    console.log(`Server Listen port :`,port);
+    console.log(`Worker PID ${process.pid} started`);
+})
